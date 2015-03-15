9d88163f2db433b9f31c98a8b3abac8eb17e9d4c
Jesse Luehrs <doy@tozt.net>
2015-03-15 04:45:09 -0400

    no reason to do this manually

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,6 @@ use serialize::base64::{ToBase64,STANDARD};
 use serialize::hex::FromHex;
 
 pub fn hex_to_base64 (hex: &str) -> String {
-    let bytes = match hex.from_hex() {
-        Ok(b) => b,
-        Err(e) => panic!("{}", e),
-    };
+    let bytes = hex.from_hex().unwrap();
     return bytes.to_base64(STANDARD);
 }
